648658ce638292c8e3e176d3d8ee94f1735c2d2a
Miguel Mejia <miguelmejia92@outlook.com>
2022-02-07 20:01:51 -0600

    Add validations for getting a new car

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+
+let cards = []
+let sum = 0
 let hasJBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
-let sumEl = document.querySelector("#sum-el")
+let sumEl = document.getElementById("#sum-el")
 let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+
+    cards.push(firstCard)
+    cards.push(secondCard)
+
+    sum = firstCard + secondCard;
     renderGame()
 }
 
@@ -33,10 +40,12 @@ function renderGame() {
 }
 
 function newCard() {    
-    let card = getRandomCard()
-    sum += card
-    cards.push(card)
-    renderGame()
+    if(isAlive && !hasJBlackJack) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
 }
 
 function getRandomCard() {
